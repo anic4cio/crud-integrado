182c3fb05cd6d721e4a95f0f294274d643e2c816
anic4cio <anicaciovinicius36@gmail.com>
2023-04-02 11:43:51 -0300

    chore: function to return all universities info

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,66 @@
 import axios from 'axios'
-import fs from 'fs'
 
-interface UniversitiesInfoObj {
+export interface UniversitiesInfo {
   state_province: string
   web_pages: string[]
   country: string
   name: string
   alpha_two_code: string
-  domais: string[]
+  domains: string[]
 }
 
 const getUniversityByCountry = async (url: string) => {
-  const response = await axios({
-    method: 'get',
-    url,
-    responseType: 'json'
-  })
-  .then((response) => response)
-
-  return response.data as UniversitiesInfoObj[]
+  try {
+    const response = await axios({
+      method: 'get',
+      url,
+      responseType: 'json'
+    })
+      .then((response) => response)
+      return response.data as UniversitiesInfo[]
+  } catch (error) {
+    console.error('Something wrong with Universities Hipolabs API')
+   throw error
+  }
 }
 
 const returnAllUniversitiesUrl = () => {
-  const argentina = 'http://universities.hipolabs.com/search?country=argentina'
-  const brazil = 'http://universities.hipolabs.com/search?country=brazil'
-  const chile = 'http://universities.hipolabs.com/search?country=chile'
-  const colombia = 'http://universities.hipolabs.com/search?country=colombia'
-  const paraguay = 'http://universities.hipolabs.com/search?country=paraguay'
-  const peru = 'http://universities.hipolabs.com/search?country=peru'
-  const suriname = 'http://universities.hipolabs.com/search?country=suriname'
-  const uruguay = 'http://universities.hipolabs.com/search?country=uruguay'
+  return {
+    argentina: 'http://universities.hipolabs.com/search?country=argentina',
+    brazil: 'http://universities.hipolabs.com/search?country=brazil',
+    chile: 'http://universities.hipolabs.com/search?country=chile',
+    colombia: 'http://universities.hipolabs.com/search?country=colombia',
+    paraguay: 'http://universities.hipolabs.com/search?country=paraguay',
+    peru: 'http://universities.hipolabs.com/search?country=peru',
+    suriname: 'http://universities.hipolabs.com/search?country=suriname',
+    uruguay: 'http://universities.hipolabs.com/search?country=uruguay'
+  }
+}
+
+const returnUniversitiesOfAllCountries = async () => {
+  const university = returnAllUniversitiesUrl()
+  const uniOfArgentina = await getUniversityByCountry(university.argentina)
+  const uniOfBrazil = await getUniversityByCountry(university.brazil)
+  const uniOfChile = await getUniversityByCountry(university.chile)
+  const uniOfColombia = await getUniversityByCountry(university.colombia)
+  const uniOfParaguay = await getUniversityByCountry(university.paraguay)
+  const uniOfPeru = await getUniversityByCountry(university.peru)
+  const uniOfSuriname = await getUniversityByCountry(university.suriname)
+  const uniOfUruguay = await getUniversityByCountry(university.uruguay)
+  return {
+    uniOfArgentina,
+    uniOfBrazil,
+    uniOfChile,
+    uniOfColombia,
+    uniOfParaguay,
+    uniOfPeru,
+    uniOfSuriname,
+    uniOfUruguay,
+  }
+}
+
+export default {
+  getUniversityByCountry,
+  returnAllUniversitiesUrl,
+  returnUniversitiesOfAllCountries
 }
