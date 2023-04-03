0e54b92c31a98fdbf2de545a39f28aea6202b6d8
anic4cio <anicaciovinicius36@gmail.com>
2023-04-03 13:21:20 -0300

    chore: post to db method

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import * as dotenv from 'dotenv'
+import { Universities } from './Universities'
 
 dotenv.config()
 
@@ -18,6 +19,34 @@ app.get('/', (req, res) => {
   res.json(
     { my_key: 'My value' }
   )
+})
+
+app.post('/universities', async (req, res) => {
+  const {
+    state_province,
+    web_pages,
+    country,
+    name,
+    alpha_two_code,
+    domains
+  } = req.body
+
+  const university = {
+    state_province,
+    web_pages,
+    country,
+    name,
+    alpha_two_code,
+    domains
+  }
+
+  try {
+    await Universities.create(university)
+    res.status(201).json({ message: 'University info suscessfully registered' })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal server error')
+  }
 })
 
 mongoose
